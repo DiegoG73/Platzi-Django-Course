ba54b868f5dcda71716150a34fa5a93518da1ff9
Diego Guzmán <oropezaeduardo442@gmail.com>
2023-07-05 20:08:05 -0600

    Terminado el primer curso de Django

--- codeapp/polls/urls.py
+++ codeapp/polls/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.IndexView.as_view(), name="index"),
+    # example: /polls/5/ Accedemos al detalle de la pregunta número 5
+    path("<int:pk>/details/evitandohardcoding", views.DetailView.as_view(), name="detail"),
+    # example: /polls/5/results/
+    path("<int:pk>/results", views.ResultView.as_view(), name="results"),
+    # example: /polls/5/vote 
+    path("<int:question_id>/vote", views.vote, name="vote"),
 ]
